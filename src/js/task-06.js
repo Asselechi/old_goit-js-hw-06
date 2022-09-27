bf1568289e3349d82ddbd8fd78fb56baf9142b31
Asselechi <104963856+Asselechi@users.noreply.github.com>
2022-09-27 23:20:24 +0600

    task 6
    task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#validation-input');
+console.log(input);
+
+console.log(input.getAttribute('data-length'));
+
+input.addEventListener('blur', eventBlur);
+
+function eventBlur(event) {
+    if (event.target.value.length == input.getAttribute('data-length')) {
+        input.classList.add('valid')
+        if (input.classList.contains('invalid')) {
+            input.classList.remove('invalid');
+        }
+    } else {
+        if (input.classList.contains('valid')) {
+            input.classList.remove('valid');
+        } input.classList.add('invalid');
+    } 
+}
