3db7a6d0990cc8ddea5c6787aa6842e9dffb5ad4
Asselechi <104963856+Asselechi@users.noreply.github.com>
2022-09-29 23:29:30 +0600

    task 9
    task 9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,8 +1,27 @@
 
 const form = document.querySelector('.login-form');
 
+const inputEl = document.querySelectorAll('input');
+console.log(inputEl);
+
 form.addEventListener('submit', formSubmit);
 
+
 function formSubmit(event) {
-    event.preventDefaul();
-}
+    event.preventDefault();
+
+    const elements = new FormData(event.currentTarget);
+    console.log(elements);
+
+    
+    
+
+};
+//     inputEl.forEach(input => {
+//     if (input.value === '') {
+//         alert('All fields should be filled')
+//     }
+// });
+
+ 
+
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
+
+const buttonEl = document.querySelector('button.change-color');
+console.log(buttonEl);
+
+const spanEl = document.querySelector('span.color');
+console.log(spanEl);
+
+buttonEl.addEventListener('click', changeColor);
+
+function changeColor () {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+}
