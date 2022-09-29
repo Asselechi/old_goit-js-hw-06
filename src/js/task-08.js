a4b4942dd8a9df10c162820ef2b0a83638d52ebc
Asselechi <104963856+Asselechi@users.noreply.github.com>
2022-09-30 00:10:11 +0600

    task 8
    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,27 +1,18 @@
 
 const form = document.querySelector('.login-form');
 
-const inputEl = document.querySelectorAll('input');
-console.log(inputEl);
-
 form.addEventListener('submit', formSubmit);
 
 
 function formSubmit(event) {
     event.preventDefault();
 
-    const elements = new FormData(event.currentTarget);
-    console.log(elements);
-
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
     
-    
-
-};
-//     inputEl.forEach(input => {
-//     if (input.value === '') {
-//         alert('All fields should be filled')
-//     }
-// });
-
- 
+    formData.forEach((email, password) => {
+        
+    });
+}
+// дальше не знаю...
 
